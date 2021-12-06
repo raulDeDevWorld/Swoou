c30145fee7a7c1a7afe1b30b15861b0c9a511659
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2021-12-06 11:47:13 -0400

    Update PageLayout.jsx

--- layouts/PageLayout.jsx
+++ layouts/PageLayout.jsx
@@ -7,7 +7,7 @@ export default function PageLayout ({ children }) {
         <Navbar />
         <main> { children } </main>
         <div className={style.vector}>
-          <button className={style.button}>Comunidad</button>
+          <button className={style.button}><a href="https://api.whatsapp.com/send?phone=+59173447725&text=buenas,%20me%20gustaria%20comunicarme%20con%20un%20%20operador(a)%20">Operador(a)</a></button>
         </div>
     </div>
   )
