eac31aacd397be0eed4cc4c7d5c42152d0c6199e
Your Name <you@example.com>
2022-01-06 19:13:04 -0300

    second commit

--- HOCs/WithoutAuth.jsx
+++ HOCs/WithoutAuth.jsx
@@ -10,7 +10,7 @@ export function WithoutAuth(Component) {
         const router = useRouter()
         useEffect(() => {
           onAuth(setUserProfile)
-          if (user) router.replace('/Home')
+          if (user) router.replace('/Welcome')
         }, [user]);
 
         return (
