53ac3f4b483dca6b2329c2e42ab8e152eafbedf4
Your Name <you@example.com>
2022-01-06 19:33:53 -0300

    sixth commit

--- HOCs/WithAuth.jsx
+++ HOCs/WithAuth.jsx
@@ -3,8 +3,9 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { useUser } from '../context/Context.js'
 import { onAuth } from '../firebase/utils'
+import { WithoutAuth } from './WithoutAuth.jsx'
 
-export function WithAuth(Component) {
+function WithAuth(Component) {
     return () => {
         const { user, setUserProfile, setUserData } = useUser()
         const router = useRouter()
@@ -21,3 +22,4 @@ export function WithAuth(Component) {
         )
     }
 }
+export default WithoutAuth(WithAuth)
