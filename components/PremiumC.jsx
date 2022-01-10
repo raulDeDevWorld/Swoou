e873af4fce3b97d3eb697e80b988ff5cc87515bd
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2022-01-10 13:31:02 -0400

    Create PremiumC.jsx

---
+++ components/PremiumC.jsx
@@ -0,0 +1,10 @@
+import style from '../styles/PremiumC.module.css'
+
+function PremiumC() {
+    return (
+            <div className={style.box}>
+                <h1 className={style.title}>Swoou Premium</h1>fvju
+                <h4 className={style.subtitle}>hazlo simple</h4>
+            </div>
+           )
+export default PremiumC
