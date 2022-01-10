a4dc32ba42b28d5a23fb148a1401cc8ed2eb4376
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2022-01-10 13:37:00 -0400

    Update PremiumC.jsx

--- components/PremiumC.jsx
+++ components/PremiumC.jsx
@@ -6,5 +6,5 @@ function PremiumC() {
                 <h1 className={style.title}>Swoou Premium</h1>fvju
                 <h4 className={style.subtitle}>hazlo simple</h4>
             </div>
-           )
+           )}
 export default PremiumC
