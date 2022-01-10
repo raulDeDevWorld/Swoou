54c1ebaeb3808ea93281c397dc20fb35e7c20fc8
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2022-01-10 17:09:25 -0400

    Update PremiumC.jsx

--- components/PremiumC.jsx
+++ components/PremiumC.jsx
@@ -3,7 +3,7 @@ import style from '../styles/PremiumC.module.css'
 function PremiumC() {
     return (
             <div className={style.box}>
-                <span className={style.title}>Swoou Premium</span></br>
+                <span className={style.title}>Swoou Premium</span><br>
                 <span className={style.subtitle}>hazlo simple</span>
                 <span className={style.subtitleTwo}>hazte premium</span>
             </div>
