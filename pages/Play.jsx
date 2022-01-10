c1229b2607d1ed4f92328d2966966084e93a271c
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2022-01-10 11:33:42 -0400

    Create Play.jsx

--- pages/Play.jsx
+++ pages/Play.jsx
@@ -67,7 +67,7 @@ if (objet !== null) {console.log(objet.nOne)}
             <div className={style.container}>
 
                 <img src={`/${userDB.avatar}.png`} className={styleH.perfil} alt="user photo" />
-                <Subtitle> Tu puedes: <br /> {`${userDB.aName.toUpperCase()}`}</Subtitle>
+                <Subtitle className={style.white}> Tu puedes: <br /> {`${userDB.aName.toUpperCase()}`}</Subtitle>
      
                 {objet !== null &&
                 <>
