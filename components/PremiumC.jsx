a831a8b26fa1edbaa24afb5bf69fd5d499f666a5
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2022-01-10 20:41:46 -0400

    Update PremiumC.jsx

--- components/PremiumC.jsx
+++ components/PremiumC.jsx
@@ -1,8 +1,13 @@
+import { useRouter } from 'next/router'
 import style from '../styles/PremiumC.module.css'
 
 function PremiumC() {
+const router = useRouter()
+function next () {
+    router.push('/Premium')
+}
     return (
-            <div className={style.box}>
+            <div className={style.box} onClick={next}>
                 <span className={style.title}>Swoou Premium</span>
                 <div className={style.cont}>
                     <span className={style.subtitle}>hazlo simple</span>
