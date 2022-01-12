0b3c88ac8b47fe757f9e8441503d16e111a8415e
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2022-01-11 23:34:34 -0400

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+export default function Doc () {
+  return (
+    <>
+      <Head>
+      {/* google adsense */}
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
+      <script dangerouslySetInnerHTML={{
+        __html: `
+            (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "pub-4940106708988712",
+            enable_page_level_ads: true
+            });
+            `,
+            }} />
+       </Head>
+    </>
+  )
+}
