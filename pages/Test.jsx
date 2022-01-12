129b7af0128eea7cd1744732d2d389a7950566f0
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2022-01-12 01:54:57 -0400

    Update Test.jsx

--- pages/Test.jsx
+++ pages/Test.jsx
@@ -1,10 +1,12 @@
 import PageLayout from '../layouts/PageLayout'
+import Adsense from '../components/Adsense'
 import { WithAuth } from '../HOCs/WithAuth'
 import style from '../styles/Home.module.css'
 
 function Test () {
     return (
     <PageLayout>
+    <Adsense/>
         <div className={style.container}>
             Test
         </div>
