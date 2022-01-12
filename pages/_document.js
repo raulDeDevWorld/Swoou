0ac2ab95294412ba382dad40b1aa51ccd134870f
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2022-01-12 00:53:24 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,16 +5,8 @@ export default function Doc () {
 return (
 /* ... */
     <Head>
-    {/* google adsense */}
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
-    <script dangerouslySetInnerHTML={{
-        __html: `
-        (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "pub-4940106708988712",
-            enable_page_level_ads: true,
-            });
-            `,
-            }} />
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4940106708988712"
+     crossorigin="anonymous"></script>
 </Head>
 
 
