16ca72bdff3de5f054872fdf70f53ad61069c276
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2022-01-12 01:49:10 -0400

    Update PageLayout.jsx

--- layouts/PageLayout.jsx
+++ layouts/PageLayout.jsx
@@ -1,10 +1,12 @@
 import Navbar from '../components/Navbar'
+import Adsense from '../components/Adsense'
 import style from '../styles/PageLayout.module.css'
 
 export default function PageLayout ({ children }) {
   return (
     <div className={style.container}>
         <Navbar />
+        <Adsense />
         <main className={style.mainContainer}> { children } </main>
         <div className={style.vector}>
           <button className={style.button}><a href="https://api.whatsapp.com/send?phone=+59173447725&text=buenas,%20me%20gustaria%20comunicarme%20con%20un%20%20operador(a)%20">Operador(a)</a></button>
