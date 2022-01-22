481767c2d67c3f5a39a5f585dcdd15c2e0c3334c
raulDeDevWorld <proyection009@gmail.com>
2022-01-22 16:07:54 -0400

    Desktop commit

--- pages/Robot.jsx
+++ pages/Robot.jsx
@@ -8,79 +8,85 @@ import { useRouter } from 'next/router'
 import { WithAuth } from '../HOCs/WithAuth'
 import style from '../styles/Robot.module.css'
 import styleH from '../styles/Home.module.css'
+import {rob} from '../utils/robot'
 
 
 
-function Play () {
+function Robot() {
     const { userDB, avatar } = useUser()
     const [mode, setMode] = useState('multiplicacion')
-    const [values, setValues] = useState({firstValue: '', secondValue: ''})
+    const [values, setValues] = useState({ firstValue: '', secondValue: '' })
     const [res, setRes] = useState(false)
-
-
+    const [obj, setObj] =useState(null)
 
     const router = useRouter()
-    
- 
-    function multiplicacion () {
+
+    function multiplicacion() {
         setMode('multiplicacion')
     }
-    function division () {
+    function division() {
         setMode('division')
     }
-    function handleInputChange (e) {
+    function handleInputChange(e) {
         e.preventDefault()
         const name = e.target.name
         const value = e.target.value
-        setValues({ ...values, [name] : value})
+        setValues({ ...values, [name]: value })
+        setObj(null)
     }
-    function resolver () {
+    function resolver() {
         setRes(true)
     }
-
   
-
+    
+    useEffect(() => {
+        values.firstValue !== '' && values.secondValue !== '' ? setObj(rob(values.firstValue, values.secondValue)): ''
+    },[values]);   
+  console.log(obj)
     return (
         <div className={style.main}>
-        {userDB !== 'loading' &&
-            <>
-            <div className={style.container}>
+            {userDB !== 'loading' &&
+                <>
+                    <div className={style.container}>
 
-                <img src="/robot.png" className={style.perfil} alt="user photo" />
-                <p className={style.greeting}> Hola, {`${userDB.aName.toUpperCase()}`} dime en que te ayudo...</p>
-             
-                <div className={style.box}>
-                <button className={`${style.button} ${mode == 'multiplicacion' ? style.right: '' }`} onClick={multiplicacion}>Multiplicación</button>
-                <button className={`${style.button} ${mode == 'division' ? style.right: '' }`} onClick={division}>División</button>
-                </div>
-                <p className={style.greeting}>Ingresa los datos</p>
-              
-                    <div className={style.box}>
-                        <input className={style.input} type="number" name="firstValue" onChange={handleInputChange} placeholder={mode == 'division'? 'Dividendo':'Multiplicando' } />
-                        <input className={style.input} type="number" name="secondValue" onChange={handleInputChange} placeholder={mode == 'division'? 'Divisor':'Multiplicador' } />
+                        <img src="/robot.png" className={style.perfil} alt="user photo" />
+                        <p className={style.greeting}> Hola, {`${userDB.aName.toUpperCase()}`} dime en que te ayudo...</p>
+
+                        <div className={style.box}>
+                            <button className={`${style.button} ${mode == 'multiplicacion' ? style.right : ''}`} onClick={multiplicacion}>Multiplicación</button>
+                            <button className={`${style.button} ${mode == 'division' ? style.right : ''}`} onClick={division}>División</button>
+                        </div>
+                        <p className={style.greeting}>Ingresa los datos</p>
+
+                        <div className={style.box}>
+                            <input className={style.input} type="number" name="firstValue" onChange={handleInputChange} placeholder={mode == 'division' ? 'Dividendo' : 'Multiplicando'} />
+                            <input className={style.input} type="number" name="secondValue" onChange={handleInputChange} placeholder={mode == 'division' ? 'Divisor' : 'Multiplicador'} />
+                        </div>
+                        <div className={style.boxMain}>
+                            {mode == 'multiplicacion' ?
+                                <div>
+                                    <span className={style.p}>{values.firstValue} </span>
+                                    <span className={`${style.p} ${res == true && values.secondValue.length < 2 ? '' : style.border}`}><span className={style.red}>{values.secondValue !== '' ? 'X' : ''}</span> {values.secondValue}</span>
+                                    {res == true && values.secondValue.length > 1 ? values.secondValue.split('').reverse().map((i, index) => <span className={style.p} key={index}>{i * values.firstValue}<span className={style.hide}>{values.secondValue.substring(0, index)} </span></span>) : ''}
+                                    {res == true && values.secondValue.length > 0 ? <span className={`${style.p} ${style.borderTop}`}>{values.firstValue * values.secondValue} </span> : ''}
+                                </div> :
+                                <div className={`${style.divisionBox} ${values.firstValue !== '' || values.secondValue !== '' ? style.display : ''}`}>
+                                    <span className={`${style.dividendo} ${values.firstValue !== '' || values.secondValue !== '' ? style.vertical : ''}`}>
+                                        <span>{values.firstValue}</span> 
+                                       {obj !== null ? obj.map((i, index)=> <span className={style.residuo} key={index}><span className={style.hideDiv}>{Math.trunc(values.firstValue / values.secondValue).toString().replace(/,/g,"").substring(0, index)}</span>{i}</span>): ''} 
+                                    </span>
+                                    {values.secondValue !== '' ? <span className={style.divisor}><span className={`${values.firstValue !== '' || values.secondValue !== '' ? style.horizontal : ''}`}>{values.secondValue}</span><span className={style.span}>{Math.trunc(values.firstValue / values.secondValue)}</span></span> : ''}
+                                </div>}
+                        </div>
+                        <button className={style.buttonGreen} onClick={resolver}>Resolver</button>
+
                     </div>
-                    <div className={style.boxMain}>
-                      {mode == 'multiplicacion' ? 
-                        <div>
-                            <span className={style.p}>{values.firstValue} </span>
-                            <span className={`${style.p} ${res == true && values.secondValue.length < 2 ? '': style.border}`}><span className={style.red}>{values.secondValue !== ''? 'X':''}</span> {values.secondValue}</span>
-                            {res == true && values.secondValue.length > 1 ?  values.secondValue.split('').reverse().map((i, index) => <span className={style.p} key={index}>{i*values.firstValue}<span className={style.hide}>{values.secondValue.substring(0, index)} </span></span>) : '' }
-                            {res == true && values.secondValue.length > 0 ? <span className={`${style.p} ${style.borderTop}`}>{values.firstValue * values.secondValue} </span>: ''}
-                        </div> :
-                        <div className={`${style.divisionBox} ${values.firstValue !== '' || values.secondValue !== ''? style.display : '' }`}>
-                            <span className={`${style.dividendo} ${values.firstValue !== '' || values.secondValue !== ''? style.vertical: ''}`}><span>{values.firstValue}</span></span>
-                            <span className={style.divisor}><span className={`${values.firstValue !== '' || values.secondValue !== ''? style.horizontal: ''}`}>{values.secondValue}</span></span>
-                            </div>}
-                    </div>
-                    <button className={style.buttonGreen} onClick={resolver}>Resolver</button> 
-              
-                </div>
-                
-           </>}
+
+                </>}
         </div>
 
     )
 
 
 }
-export default WithAuth(Play)
+export default WithAuth(Robot)
---
+++ utils/robot.js
@@ -0,0 +1,29 @@
+function rob(v1, v2) {
+
+    const residuo = []
+    const cifra = []
+
+    function robb (v1, v2) {
+
+        const vl1 = v1.length
+        const vl2 = v2.length
+
+        function alg(v1, v2, c, d) {
+            const cifras = v1.substring(0, d) >= v2 ? v1.substring(0, d) : v1.substring(0, d + 1)
+            const q = Math.trunc(cifras / v2)
+            const r = cifras - (q * v2)
+            residuo.push(r)
+            cifra.push(cifras)
+            console.log(cifra)
+            cifras.length < c ? robb(`${r}${v1.substring(cifras.length, c)}`, v2) : ''
+        }
+        alg(v1, v2, vl1, vl2)
+    }
+    robb(v1, v2)
+    cifra.shift()
+    const item = residuo.pop()
+    cifra.push(item)
+    return cifra 
+}
+
+export {rob}
