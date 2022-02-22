25209a6037dc299046d5d92d151ad68f54c1fe82
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2022-02-22 19:04:14 -0400

    Update PageEspecial.jsx
    CC

--- layouts/PageEspecial.jsx
+++ layouts/PageEspecial.jsx
@@ -1,14 +1,18 @@
-import Navbar from '../components/Navbar'
-import style from '../styles/PageLayout.module.css'
+import Head from 'next/head'
+
 
 export default function PageEspecial ({ children }) {
   return (
-    <div className={style.container}>
-        <Navbar />
-        <main className={style.mainContainerEspecial}> { children } </main>
-        <div className={style.vector}>
-          <button className={style.button}><a href="https://api.whatsapp.com/send?phone=+59173447725&text=buenas,%20me%20gustaria%20comunicarme%20con%20un%20%20operador(a)%20">Operador(a)</a></button>
-        </div>
-    </div>
+    <>
+        <Head>
+        <title>Swoou</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#01afff" />
+        <meta name="msapplication-navbutton-color" content="#01afff" /> 
+        <meta name="apple-mobile-web-app-status-bar-style" content="#01afff" /> 
+        </Head>
+        <main> { children } </main>
+  
+    </>
   )
 }
