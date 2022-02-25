25aed3009d03caef065e4c38d6fb2ef85fecf540
raulDeDevWorld <71567510+raulDeDevWorld@users.noreply.github.com>
2022-02-24 22:07:05 -0400

    Update InitialLayout.jsx
    CC

--- layouts/InitialLayout.jsx
+++ layouts/InitialLayout.jsx
@@ -24,9 +24,9 @@ export default function InitialLayout ({ children }) {
         <meta name="theme-color" content="#000000" />
         <meta name="msapplication-navbutton-color" content="#000000" /> 
         <meta name="apple-mobile-web-app-status-bar-style" content="#000000" /> 
-        <meta name="description" content="Desarrollando tecnología para hacer de un mundo complicado un mundo más sencillo">
-        <meta name="keywords" content="Swoou, Matemáticas, Multiplicación, Primaria, desarrollador web, ">
-        <meta name="author" content="Raul Choque Romero">
+        <meta name="description" content="Desarrollando tecnología para hacer de un mundo complicado un mundo más sencillo" />
+        <meta name="keywords" content="Swoou, Matemáticas, Multiplicación, Primaria, desarrollador web" />
+        <meta name="author" content="Raul Choque Romero" />
     </Head>
     <div className={style.container}>
       {/* <span className={style.msg}>{msg}</span>  */}
